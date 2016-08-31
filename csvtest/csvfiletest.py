03f9bd4e7223a194124cdf57b96845050f84b2f4
joseoronos <jose.oronos@gmail.com>
2016-08-31 15:06:51 +1000

    csv tings innit
    csv tings innit

--- csvtest/csvfiletest.py
+++ csvtest/csvfiletest.py
@@ -1,47 +1,15 @@
 import csv
 
-"""
-def main():
-    with open('items.csv') as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        dates = []
-        colors = []
-        for row in readCSV:
-            color = row[3]
-            date = row[0]
+f = open("items.csv")
 
-            dates.append(date)
-            colors.append(color)
+csv_f = csv.reader(f)
 
-        print(dates)
-        print(colors)
+items = []
 
-main()
-"""
+for row in csv_f:
+    items.append(row)
+    #print(row[0])
 
-"""
-def main():
-    with open('items.csv') as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        count = 0
-        for row in readCSV:
-            product = row[0]
-            product_cost = row[1]
-            priority = row[2]
-            print("{}.{} {} {}".format(count, product, product_cost, priority))
-            count += 1
-main()
-"""
+print(items)
 
-"""
-def main():
-    with open('items.csv') as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        for row in readCSV:
-            signal = row[3]
-            if signal == "c":
-                print(row)
-main()
-"""
-                
-
+f.close()
