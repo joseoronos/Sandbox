415d344d33d54d25231d451aed795b2f521a1181
Jose Oronos <jose.oronos@gmail.com>
2016-08-31 16:37:07 +1000

    mad ting got the ting werkin
    mad ting got the ting werkin

--- csvtest/csvfiletest.py
+++ csvtest/csvfiletest.py
@@ -1,15 +1,69 @@
 import csv
 
-f = open("items.csv")
 
-csv_f = csv.reader(f)
+def main():
+    print("Shopping List 0.1 - by Jose Oronos")
 
-items = []
+    f = open("items.csv")
+    csv_f = csv.reader(f)
+    items = []
+    for row in csv_f:
+        items.append(row)
 
-for row in csv_f:
-    items.append(row)
-    #print(row[0])
+    print("Menu: ")
+    print("R - List required items")
+    print("C - List completed items")
+    print("A - Add new item")
+    print("M - Mark an item as completed")
+    print("Q - Quit")
+    user_choice = input("Please select an option: ").lower()
 
-print(items)
+    while user_choice != "q":
+        if user_choice == "r":
+            print("You chose R")
+            req_list = []
+            for thing in items:
+                if thing[3] == "r":
+                    req_list.append(thing)
+            if len(req_list) == 0:
+                print("No required items")
+            else:
+                print(req_list)
+        elif user_choice == "c":
+            print("You chose C")
+            cmp_list = []
+            for thing in items:
+                if thing[3] == "c":
+                    cmp_list.append(thing)
+            if len(cmp_list) == 0:
+                print("No completed items")
+            else:
+                print(cmp_list)
+        elif user_choice == "a":
+            print("You chose A")
+            new_product_info = []
+            user_product = input("Item name: ")
+            product_price = input("Prioe: $")
+            product_priority = input("Priority: ")
+            req = "r"
+            new_product_info.append(user_product)
+            new_product_info.append(product_price)
+            new_product_info.append(product_priority)
+            new_product_info.append(req)
+            items.append(new_product_info)
+            print("{}, ${} (priority {}) added to the shopping list".format(user_product, product_price, product_priority))
+        elif user_choice == "m":
+            print("You chose M")
+        else:
+            print("Invalid option, please choose again")
+        print("Menu: ")
+        print("R - List required items")
+        print("C - List completed items")
+        print("A - Add new item")
+        print("M - Mark an item as completed")
+        print("Q - Quit")
+        user_choice = input("Please select an option: ").lower()
+    print("Done")
 
-f.close()
+
+main()
--- csvtest/sandbox.py
+++ csvtest/sandbox.py
@@ -1,70 +1,15 @@
 import csv
 
+f = open("items.csv")
 
+csv_f = csv.reader(f)
 
-def load_data():
-    with open("items.csv", "r") as test_file:
-        test_file_reader = csv.reader(test_file)
-        file_list = []
-        for row in test_file_reader:
-            file_list += [row]
+items = []
 
-    test_file.close()
-    return file_list
+for row in csv_f:
+    items.append(row)
+    #print(row[0])
 
+print(items)
 
-def show_required_list(file_list):
-    for item in file_list:
-        if item[3] == "r":
-            print(item)
-
-
-def show_completed_list(file_list):
-    for item in file_list:
-        if item[3] == "c":
-            print(item)
-
-
-def main():
-    print("Shopping List 0.1 - by Jose Oronos")
-    print("Menu: ")
-    print("R - List required items")
-    print("C - List completed items")
-    print("A - Add new item")
-    print("M - Mark an item as completed")
-    print("Q - Quit")
-    user_choice = input("Please select an option: ").lower()
-    while user_choice != "q":
-        if user_choice == "r":
-            print("You chose R")
-            required_items = load_data()
-            show_required_list(required_items)
-        elif user_choice == "c":
-            print("You chose C")
-            completed_items = load_data()
-            show_completed_list(completed_items)
-        elif user_choice == "a":
-            print("You chose A")
-            ting_name = input("Enter ting: ")
-            ting_price = input("Enter price of ting: ")
-            ting_priority = input("Enter priority of ting: ")
-            req = "r"
-            with open("items.csv", "a") as test_file:
-                test_file_writer = csv.writer(test_file)
-                test_file_writer.writerow([ting_name, ting_price, ting_priority, req])
-            test_file.close()
-        elif user_choice == "m":
-            print("You chose M")
-        else:
-            print("Invalid option, please choose again")
-        print("Menu: ")
-        print("R - List required items")
-        print("C - List completed items")
-        print("A - Add new item")
-        print("M - Mark an item as completed")
-        print("Q - Quit")
-        user_choice = input("Please select an option: ").lower()
-    print("Done")
-
-
-main()
+f.close()
